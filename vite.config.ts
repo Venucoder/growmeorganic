a08dec2c314c7eca4894c59c4abf3abe92a2aaaa
Venucoder <kallamvgreddy123@gmail.com>
2023-07-27 15:12:29 +0530

    Add project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    strictPort: false,
+    fs: {
+      strict: false,
+    },
+  },
+};
